c282a010f09e0b8de81c36a320c66d840e35bc25
Allen Lucke <allenlucke@gmail.com>
2020-04-23 15:24:08 -0500

    added nested for loop of multiDArray to Explore

--- src/main/java/Arrays/Explore.java
+++ src/main/java/Arrays/Explore.java
@@ -4,6 +4,31 @@ public class Explore {
     public static void main(String[] args) {
         String[] friends = {"Ben", "Alena", "Jerry"};
 
+        //Muti-Dimensional Score Card array
+        int[][] scoreCards = {
+            // Ben's
+            {1, 2, 4, 2, 6, 5, 4, 3, 3, 2, 5, 7, 2, 7, 8, 4, 3, 2},
+            // Alena's
+            {2, 3, 5, 1, 1, 2, 3, 1, 1, 2, 4, 1, 3, 3, 2, 6, 3, 2},
+            // Pasan's
+            {4, 4, 2, 1, 2, 2, 1, 4, 2, 2, 2, 3, 2, 5, 8, 1, 2, 2}
+        };
+
+        // for each friend
+        for(int i = 0; i < friends.length; i++) {
+            // print their name
+            System.out.printf("%s %n------------%n",
+                    friends[i]);
+            // for each hole
+            for(int j = 0; j < scoreCards[i].length; j++) {
+                // print their score
+                System.out.printf("Hole #%d: %d %n",
+                        j + 1,
+                        scoreCards[i][j]);
+            }
+        }
+
+
         //Iterating using the enhanced for loop
         for (String friend : friends) {
             System.out.printf("Hey %s! The movie starts at 7, See you there! %n", friend);
---
+++ src/main/java/Arrays/scratch.java
@@ -0,0 +1,14 @@
+//int[][] scoreCards = {
+//        // Ben's
+//        {1, 2, 4, 2, 6, 5, 4, 3, 3, 2, 5, 7, 2, 7, 8, 4, 3, 2},
+//// Alena's
+//        {2, 3, 5, 1, 1, 2, 3, 1, 1, 2, 4, 1, 3, 3, 2, 6, 3, 2},
+//// Pasan's
+//        {4, 4, 2, 1, 2, 2, 1, 4, 2, 2, 2, 3, 2, 5, 8, 1, 2, 2}
+//};
+//
+////Yields results from Alena's fourth hole
+//scoreCards[1][3];
+//
+////Yields results from Pasan's eighteenth hole
+//scoreCards[2][17];
