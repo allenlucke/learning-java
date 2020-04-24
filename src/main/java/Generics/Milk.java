065eb3814145720ba5699aca6a6b96f213392982
Allen Lucke <allenlucke@gmail.com>
2020-04-24 15:51:04 -0500

    added generics package

---
+++ src/main/java/Generics/Milk.java
@@ -0,0 +1,9 @@
+package Generics;
+
+class Milk {
+    void drink() {
+        System.out.println("You drink the milk.");
+    }
+}
+
+
---
+++ src/main/java/Generics/Oranges.java
@@ -0,0 +1,7 @@
+package Generics;
+
+class Oranges {
+    void juggle() {
+        System.out.println("You drop the oranges on the ground.");
+    }
+}
