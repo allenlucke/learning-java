b647afb81823654f066a0ebc468015bfb4bbd4f1
Allen Lucke <allenlucke@gmail.com>
2020-04-24 12:27:44 -0500

    added Inheritance/main

---
+++ src/main/java/Inheritance/Main.java
@@ -0,0 +1,22 @@
+package Inheritance;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.makeSound();
+    }
+}
+
+class Animal {
+    String sound = "";
+    void makeSound() {
+        System.out.println(sound);
+    }
+}
+
+class Dog extends Animal {
+    Dog() {
+        sound = "bark";
+    }
+}
