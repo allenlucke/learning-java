5847b17f94d7ae9799f45c23ee7776c94a63c43f
Allen Lucke <allenlucke@gmail.com>
2020-04-27 14:21:31 -0500

    added list functions to lists/main

--- src/main/java/Lists/Main.java
+++ src/main/java/Lists/Main.java
@@ -19,5 +19,19 @@ public class Main {
 
         String jerome = groceryLine.get(0);
         System.out.println(jerome);
+
+        int samIndex = groceryLine.indexOf("Sam");
+        System.out.println(samIndex);
+
+        int pamIndex = groceryLine.indexOf("Pam");
+        //Will return -1 bc Pam is not in the list
+        System.out.println(pamIndex);
+
+        System.out.println(groceryLine.size());
+
+        for (String name : groceryLine) {
+            System.out.println(name);
+        }
+
     }
 }
